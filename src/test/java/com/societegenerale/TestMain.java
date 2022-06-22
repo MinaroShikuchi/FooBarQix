4b300afc0389a612a2780e2bdb010469f63e3b7c
Minaro <paturetromain@gmail.com>
2022-06-22 22:41:40 +0200

    Add Step1 unit tests

---
+++ src/test/java/com/societegenerale/TestMain.java
@@ -0,0 +1,28 @@
+package com.societegenerale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestMain {
+    @Test
+    void computeStep1() {
+        assertEquals("1", Main.compute("1"));
+        assertEquals("1", Main.compute("1"));
+        assertEquals("2", Main.compute("2"));
+        assertEquals("FooFoo", Main.compute("3"));
+        assertEquals("4", Main.compute("4"));
+        assertEquals("BarBar", Main.compute("5"));
+        assertEquals("Foo", Main.compute("6"));
+        assertEquals("QixQix", Main.compute("7"));
+        assertEquals("8", Main.compute("8"));
+        assertEquals("Foo", Main.compute("9"));
+        assertEquals("Bar", Main.compute("10"));
+        assertEquals("Foo", Main.compute("13"));
+        assertEquals("FooBarBar", Main.compute("15"));
+        assertEquals("FooQix", Main.compute("21"));
+        assertEquals("FooFooFoo", Main.compute("33"));
+        assertEquals("FooBar", Main.compute("51"));
+        assertEquals("BarFoo", Main.compute("53"));
+    }
+}
