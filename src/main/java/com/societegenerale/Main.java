b091f3a8d3e05681afb6903006c398f341f9d481
Minaro <paturetromain@gmail.com>
2022-06-22 22:42:33 +0200

    Add compute function

--- src/main/java/com/societegenerale/Main.java
+++ src/main/java/com/societegenerale/Main.java
@@ -1,6 +1,34 @@
 package com.societegenerale;
 
 public class Main {
+    public static String compute(String myString) {
+        String res = "";
+        int myInt =  Integer.parseInt(myString);
+
+        if (myInt % 3 == 0) {
+            res = "Foo";
+        }
+        if (myInt % 5 == 0) {
+            res += "Bar";
+        }
+        if (myInt % 7 == 0) {
+            res += "Qix";
+        }
+
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == '3') {
+               res += "Foo";
+            } else if (myString.charAt(i) == '5') {
+                res += "Bar";
+            } else if (myString.charAt(i) == '7') {
+                res += "Qix";
+            }
+        }
+
+
+        if (res == "") return myString;
+        return res;
+    }
     public void main(String[] args) {
         System.out.println("Hello");
     }
